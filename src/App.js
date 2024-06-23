0bc5a04c521171f9d0cac68309b6d9e264b91afd
Your Name <your.name@example.com>
2024-06-23 16:16:24 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import CardsDetails from './components/CardsDetails';
+import Cards from './components/Cards';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Cards />} />
+          <Route path="/cart" element={<CardsDetails />} />
+          {/* <Route path="/Edit/:id" element={<Edit />} /> */}
+          {/* <Route path="/Details/:id" element={<Details />} /> */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
